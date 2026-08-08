ebb0791cd950b0a98487742b56c7e27ee7157b33
enesyaprak <enes@Enes.localdomain>
2026-08-08 14:35:52 +0300

    ilk pipeline

---
+++ hesap.py
@@ -0,0 +1,7 @@
+def topla(a, b):
+    return a + b
+
+def bol(a, b):
+    if b == 0:
+        raise ValueError("Sifira bolunemez")
+    return a / b
---
+++ test_hesap.py
@@ -0,0 +1,12 @@
+import pytest
+from hesap import topla, bol
+
+def test_topla():
+    assert topla(2, 3) == 5
+
+def test_bol():
+    assert bol(10, 2) == 5
+
+def test_sifira_bolme():
+    with pytest.raises(ValueError):      # "bu kod hata fırlatmalı" testi
+        bol(5, 0)
